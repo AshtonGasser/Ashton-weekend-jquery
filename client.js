a13dba2faa438cf96f0d03f4ed1441994174db7f
Ashton Gasser <ashtongasser@gmail.com>
2021-05-15 11:33:21 -0500

    got employee info to push to DOM

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 $(document).ready(readyNow);
 
-
+const employeeArray = []
 
 function readyNow() {
     console.log('jQ ready')
 
 
-    $('#submit').on('click', onClick)
-    $('#delete').on('click', '.deleteBtn', onDelete); 
+    $('#submit').on('click', onClick, addEmployee)
+    $('#delete').on('click', '.deleteBtn', onDelete);
 }
 
 function onClick() {
@@ -15,19 +15,71 @@ function onClick() {
     console.log('CLICK!')
 }
 
-function onDelete(){
+function onDelete() {
 
     console.log("BEGON")
+
+    $(this).closest('tr').remove();
     //$(this).closest('box').remove();
+    //<td> <button class = "btn delete">Delete</button> </td>
 }
 
-let employee = {
-    name : '',
-    lastName : '',
-    ID : '',
-    Title : '',
-    annualSalary : ''
+function addEmployee() {
 
+    //console.log('employee info added')
+
+    let employee = {
+        name: $('#nameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        ID: $('#idIn').val(),
+        Title: $('#titleIn').val(),
+        annualSalary: $('#annualSalaryIn').val()
+
+
+
+    }
+    
+    employeeArray.push(employee)
+
+    //if()
+
+    $('.table').append(`
+        <tr> 
+            <td>${employee.name}</td>
+            <td>${employee.lastName}</td
+            <td>${employee.ID}</td
+            <td>${employee.Title}</td
+            <td>${employee.annualSalary}</td
+            <td>${employee}</td
+            <td> <button class = deleteBtn"Delete</button</td
+        </tr>
+        
+        
+        
+    `)
+
+    //clear input
+
+    $('#nameIn').val('');
+    $('#lastNameIn').val('');
+    $('#idIn').val('');
+    $('#titleIn').val('');
+    $('#annualSalaryIn').val('');
+
+    
+    console.log(employee.name, employee.lastName, employee.ID, employee.Title, employee.annualSalary);
 
 
 }
+
+
+
+// A 'Submit' button should collect the form information, 
+//  store the information to calculate monthly costs, 
+// append information to the DOM and clear the input fields. 
+// Using the stored information,
+//  calculate monthly costs and append this to the to DOM. If the total monthly cost exceeds $20,000, 
+//  add a red background color to the total monthly cost.
+
+// Create a delete button that removes an employee from the DOM.
+//  For Base mode, it does **not** need to remove that Employee's salary from the reported total.
